363d5ecc8573a72299cb39a6bb3ed95de30884d6
jacekpotapczuk <jacekpotapczuk@gmail.com>
2020-08-29 12:42:38 +0200

    zad 8,9

--- zad1/shaders/Basic.shader
+++ zad1/shaders/Basic.shader
@@ -2,7 +2,6 @@
 #version 150
 
 in vec3 position;
-in vec3 vertexColor;
 
 uniform mat4 u_model_matrix;
 
@@ -17,11 +16,10 @@ out vec3 fragmentColor;
 
 void main()
 {
+	fragmentColor = (position + 1.0) / 2.0;
 	gl_Position = u_projection_matrix * u_view_matrix * u_model_matrix * u_rotate_z_matrix * u_rotate_y_matrix * u_rotate_x_matrix * vec4(position, 1.0f);
 
-	fragmentColor = vertexColor;
 };
-
 
 
 #shader fragment
@@ -31,10 +29,8 @@ in vec3 fragmentColor;
 
 out vec4 out_color;
 
-uniform vec4 u_color;
 
 void main()
 {
-	//out_color = u_color;
 	out_color = vec4(fragmentColor, 1.0);
 };
--- zad1/zad1.cpp
+++ zad1/zad1.cpp
@@ -16,15 +16,22 @@ void reshape(int, int);
 void timer(int);
 void mouse(int, int, int, int);
 void mouse_active_motion(int, int);
-void DrawCube(GLfloat, GLfloat, GLfloat, GLfloat);
+void key_handler(unsigned char, int, int);
 
-
-int color_loc;
 
 GLuint shader;
-float angle_x = 0.0;
-float angle_y = 0.0;
-float angle_z = 0.0;
+bool auto_rotate = false;
+static float rotation[] = { 0.0, 0.0, 0.0 };
+
+int loc_rotate_x, loc_rotate_y, loc_rotate_z;
+
+
+static float mouse_start_pos[] = { -1.0, -1.0 };
+static float mouse_move_vector[] = { 0.0, 0.0 };
+static float mouse_move_vector_norm[] = { 0.0, 0.0 };
+
+bool left_mouse_button_pressed = false;
+bool mouse_rotation_enabled = true;
 
 
 static GLuint CompileShader(GLuint type, const std::string& source) {
@@ -110,12 +117,11 @@ void display()
 
     glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    //glDrawArrays(GL_TRIANGLES, 0, 3);
 
-  /*  glDrawElements(GL_TRIANGLES, 12 * 3 , GL_UNSIGNED_INT, nullptr);
-    */
+    //glDrawElements(GL_TRIANGLES, 12 * 3 , GL_UNSIGNED_INT, nullptr);
+    
     glDrawArrays(GL_TRIANGLES, 0, 12 * 3);
-    //DrawCube(0.0, 0.0, 0.0, 1.0);
+
 
     glutSwapBuffers();
 }
@@ -141,50 +147,18 @@ int main(int argc, char** argv)
         return 0;
     }
 
-    // todo: dowiedziec sie co to i po co to
     GLuint vao;
     glGenVertexArrays(1, &vao);
     glBindVertexArray(vao);
 
-    GLuint vbo; // vertex buffer - przechowuje dane o obiektach ktore rysujemy, jest to odnosnik do miejsca gdzie sa one przechowywane na GPU
-    glGenBuffers(1, &vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
 
-    //static const GLfloat positions[] = { 0.0f, 0.0f, 0.0f,
-    //                                     1.0f, 0.0f, 0.0f,
-    //                                     1.0f, 1.0f, 0.0f,
-    //                                     0.0f, 1.0f, 0.0f,
-    //                                     0.0f, 0.0f, -1.0f,
-    //                                     1.0f, 0.0f, -1.0f,
-    //                                     1.0f, 1.0f, -1.0f,
-    //                                     0.0f, 1.0f, -1.0f };
-    //glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
-    //GLuint indices[] = {
-    //    0, 1, 2,
-    //    0, 2, 3,
-    //    3, 2, 6,
-    //    3, 6, 7,
-    //    1, 5, 6,
-    //    1, 6, 2,
-    //    5, 4, 7,
-    //    5, 7, 6,
-    //    0, 4, 3,
-    //    4, 7, 3,
-    //    1, 5, 7,
-    //    1, 4, 0
-    //};
-
-    //GLuint ibo; // index buffer
-    //glGenBuffers(1, &ibo);
-    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-    //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-    static const GLfloat g_vertex_buffer_data[] = {
-    -1.0f,-1.0f,-1.0f, // triangle 1 : begin
+    static const GLfloat positions[] = {
+    -1.0f,-1.0f,-1.0f, 
     -1.0f,-1.0f, 1.0f,
-    -1.0f, 1.0f, 1.0f, // triangle 1 : end
-    1.0f, 1.0f,-1.0f, // triangle 2 : begin
+    -1.0f, 1.0f, 1.0f, 
+    1.0f, 1.0f,-1.0f, 
     -1.0f,-1.0f,-1.0f,
-    -1.0f, 1.0f,-1.0f, // triangle 2 : end
+    -1.0f, 1.0f,-1.0f,
     1.0f,-1.0f, 1.0f,
     -1.0f,-1.0f,-1.0f,
     1.0f,-1.0f,-1.0f,
@@ -220,68 +194,18 @@ int main(int argc, char** argv)
     GLuint vertexBuffer;
     glGenBuffers(1, &vertexBuffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-
-    static const GLfloat g_color_buffer_data[] = {
-1.0f, 1.0f, 1.0f,
-1.0f, 1.0f, 1.0f,
-1.0f, 1.0f, 1.0f,
-1.0f, 1.0f, 1.0f,
-1.0f, 1.0f, 1.0f,
-1.0f, 1.0f, 1.0f,
-1.0f, 1.0f, 1.0f,
-1.0f, 1.0f, 1.0f,
-1.0f, 1.0f, 1.0f,
-1.0f, 1.0f, 1.0f,
-1.0f, 1.0f, 1.0f,
-1.0f, 1.0f, 1.0f,
-1.0f, 1.0f, 1.0f,
-1.0f, 1.0f, 1.0f,
-1.0f, 1.0f, 1.0f,
-1.0f, 1.0f, 1.0f,
-1.0f, 1.0f, 1.0f,
-1.0f, 1.0f, 1.0f,
-1.0f, 1.0f, 1.0f,
-1.0f, 1.0f, 1.0f,
-1.0f, 1.0f, 1.0f,
-1.0f, 1.0f, 1.0f,
-1.0f, 1.0f, 1.0f,
-1.0f, 1.0f, 1.0f,
-1.0f, 1.0f, 1.0f,
-1.0f, 1.0f, 1.0f,
-1.0f, 1.0f, 1.0f,
-1.0f, 1.0f, 1.0f,
-1.0f, 1.0f, 1.0f,
-1.0f, 1.0f, 1.0f,
-1.0f, 1.0f, 1.0f,
-1.0f, 1.0f, 1.0f,
-1.0f, 1.0f, 1.0f,
-1.0f, 1.0f, 1.0f,
-1.0f, 1.0f, 1.0f,
-1.0f, 1.0f, 1.0f
-    };
-
-    //GLuint colorbuffer;
-    //glGenBuffers(1, &colorbuffer);
-    //glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
-    //glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
-    //glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
 
 
     ShaderProgramSource source = ParseShader("shaders/Basic.shader");
-
-    // vertex shader - called for each vertex, used to tell where you want it to be on the screen, it's called before fragment shader
-    // fragment shader - called for each pixel that needs to be rasterized (drawn on screen), primally it's used to determine color of each pixel
 
     shader = CreateShader(source.VertexSource, source.FragmentSource);
     glUseProgram(shader);
 
 
     GLint posAttrib = glGetAttribLocation(shader, "position");
-    GLint colorAttrib = glGetAttribLocation(shader, "vertexColor");
     //odblokowanie atrybutow
     glEnableVertexAttribArray(posAttrib);
-    glEnableVertexAttribArray(colorAttrib);
 
     glVertexAttribPointer(  // definiuje jak ulozone sa dane w naszych wierzcholkach
         posAttrib,      // pozycja atrybutu
@@ -292,23 +216,10 @@ int main(int argc, char** argv)
         0               // wskaznik do pierwszego atrybutu, jeśli zero to od początku (ile bajtow musimy sie przesunac do wskazanego atrybutu)
     );
 
-    glVertexAttribPointer(
-        colorAttrib,                                // attribute. No particular reason for 1, but must match the layout in the shader.
-        3,                                // size
-        GL_FLOAT,                         // type
-        GL_FALSE,                         // normalized?
-        0,                                // stride
-        (void*)0                          // array buffer offset
-    );
-
-    color_loc = glGetUniformLocation(shader, "u_color");
-    glUniform4f(color_loc, 0.8, 0.2, 0.8, 1.0);
-
-
-    float model_matrix[16] = { 4.0, 0.0, 0.0, 0.0,
+    float model_matrix[16] = { 2.0, 0.0, 0.0, 0.0,
                                0.0, 2.0, 0.0, 0.0,
                                0.0, 0.0, 1.0, 0.0,
-                               -0.5, -0.5, -1.0, 1.0 };
+                               -0.5, -0.5, -20.0, 1.0 };
 
 
     float view_matrix[16] = { 1.0, 0.0, 0.0, 0.0, // nie uzywam kamery, ale w razie potrzeby jest taka mozliwosc
@@ -316,27 +227,42 @@ int main(int argc, char** argv)
                               0.0, 0.0, 1.0, 0.0,
                               0.0, 0.0, 0.0, 1.0 };
 
-    // orthographic projection
-    float r = 10.0;   // right
-    float l = -10.0;  // left
-    float b = -10.0;  // bottom
-    float t = 10.0;   // top
-    float f = 100.0;  // far
-    float n = 0.1;    // near
+    //// orthographic projection
+    //float r = 10.0;   // right
+    //float l = -10.0;  // left
+    //float b = -10.0;  // bottom
+    //float t = 10.0;   // top
+    //float f = 100.0;  // far
+    //float n = 0.1;    // near
 
-    float projection_matrix[16] = { 2.0 / (r - l), 0.0, 0.0, -(r + l)/(r - l),
-                                    0.0, 2.0 / (t - b), 0.0, -(t + b) / (t - b),
-                                    0.0, 0.0, -2.0 / (f - n), -(f + n) / (f - n),
-                                    0.0, 0.0, 0.0, 1.0 };
+    //float projection_matrix[16] = { 2.0 / (r - l), 0.0, 0.0, -(r + l)/(r - l),
+    //                                0.0, 2.0 / (t - b), 0.0, -(t + b) / (t - b),
+    //                                0.0, 0.0, -2.0 / (f - n), -(f + n) / (f - n),
+    //                                0.0, 0.0, 0.0, 1.0 };
+
+    // perspective projection
+    float fov = 60.0;
+    float n = 0.1;
+    float f = 100.0;
+    float s = 1.0 / (tanf((fov / 2.0) * (3.1415 / 180.0)));
+
+    float projection_matrix[16] = { s, 0.0, 0.0, 0.0,
+                                    0.0, s, 0.0, 0.0,
+                                    0.0, 0.0, - (f)/(f - n), -1.0,
+                                    0.0, 0.0, - (f*n)/(f - n), 0.0 };
 
     int loc_model = glGetUniformLocation(shader, "u_model_matrix");
     int loc_view = glGetUniformLocation(shader, "u_view_matrix");
     int loc_projection = glGetUniformLocation(shader, "u_projection_matrix");
 
-
     glUniformMatrix4fv(loc_model, 1, GL_FALSE, model_matrix);
     glUniformMatrix4fv(loc_view, 1, GL_FALSE, view_matrix);
     glUniformMatrix4fv(loc_projection, 1, GL_FALSE, projection_matrix);
+
+
+    loc_rotate_x = glGetUniformLocation(shader, "u_rotate_x_matrix");
+    loc_rotate_y = glGetUniformLocation(shader, "u_rotate_y_matrix");
+    loc_rotate_z = glGetUniformLocation(shader, "u_rotate_z_matrix");
 
 
     // specify callback functions
@@ -344,6 +270,7 @@ int main(int argc, char** argv)
     glutReshapeFunc(reshape);
     glutMouseFunc(mouse);
     glutMotionFunc(mouse_active_motion);
+    glutKeyboardFunc(key_handler);
     glutTimerFunc(0, timer, 0);  // call timer function immediately
 
     glutMainLoop();
@@ -351,17 +278,50 @@ int main(int argc, char** argv)
 
     glDeleteProgram(shader);
 
-    glDeleteBuffers(1, &vbo);
+    //glDeleteBuffers(1, &vbo);
     glDeleteVertexArrays(1, &vao);
     return 0;
 }
 
+void key_handler(unsigned char key, int x, int y) {
+    if (key == 'q') {
+        if (auto_rotate)
+            auto_rotate = false;
+        else
+            auto_rotate = true;
+    }
+
+}
+
 void mouse(int button, int state, int x, int y) {
- 
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+        mouse_start_pos[0] = x;
+        mouse_start_pos[1] = y;
+        left_mouse_button_pressed = true;
+
+    }
+    else if (button == GLUT_LEFT_BUTTON && state == GLUT_UP) {
+        left_mouse_button_pressed = false;
+    }
 }
 
 void mouse_active_motion(int x, int y) {
-  
+    if (mouse_rotation_enabled && left_mouse_button_pressed) {
+        mouse_move_vector[0] = mouse_start_pos[0] - x;
+        mouse_move_vector[1] = mouse_start_pos[1] - y;
+
+        mouse_start_pos[0] = x;
+        mouse_start_pos[1] = y;
+
+        mouse_move_vector_norm[0] = mouse_move_vector[0] / 720.0;
+        mouse_move_vector_norm[1] = mouse_move_vector[1] / 720.0;
+
+        mouse_move_vector_norm[0] *= 2 * 3.1415;
+        mouse_move_vector_norm[1] *= 2 * 3.1415;
+
+        rotation[1] += -mouse_move_vector_norm[0];
+        rotation[0] += mouse_move_vector_norm[1];
+    }
 }
 
 void reshape(int w, int h) // called at the start and whenever window is reshaped
@@ -373,87 +333,28 @@ void timer(int)
 {
     glutPostRedisplay();  // call display function
     glutTimerFunc(1000 / 60, timer, 0);  // calls itself to keep 60 FPS
-
-    angle_x += 0.01;
-    angle_y += 0.02;
-    angle_z -= 0.015;
+    if (auto_rotate) {
+        rotation[0] += 0.001;
+        rotation[1] += 0.02;
+        rotation[2] -= 0.015;
+    }
 
     float rotate_x_matrix[16] = { 1.0, 0.0, 0.0, 0.0,
-                                  0.0, cosf(angle_x), -sinf(angle_x), 0.0,
-                                  0.0, sinf(angle_x), cosf(angle_x), 0.0,
+                                  0.0, cosf(rotation[0]), -sinf(rotation[0]), 0.0,
+                                  0.0, sinf(rotation[0]), cosf(rotation[0]), 0.0,
                                   0.0, 0.0, 0.0, 1.0 };
 
-    float rotate_y_matrix[16] = { cosf(angle_y), 0.0, -sinf(angle_y), 0.0,
+    float rotate_y_matrix[16] = { cosf(rotation[1]), 0.0, -sinf(rotation[1]), 0.0,
                               0.0, 1.0, 0.0, 0.0,
-                              sinf(angle_y), 0.0, cosf(angle_y), 0.0,
+                              sinf(rotation[1]), 0.0, cosf(rotation[1]), 0.0,
                               0.0, 0.0, 0.0, 1.0 };
 
-    float rotate_z_matrix[16] = { cosf(angle_z), -sinf(angle_z), 0.0, 0.0,
-                                  sinf(angle_z), cosf(angle_z), 0.0, 0.0,
+    float rotate_z_matrix[16] = { cosf(rotation[2]), -sinf(rotation[2]), 0.0, 0.0,
+                                  sinf(rotation[2]), cosf(rotation[2]), 0.0, 0.0,
                                   0.0, 0.0, 1.0, 0.0,
                                   0.0, 0.0, 0.0, 1.0 };
 
-    int loc_rotate_x = glGetUniformLocation(shader, "u_rotate_x_matrix");
     glUniformMatrix4fv(loc_rotate_x, 1, GL_FALSE, rotate_x_matrix);
-
-    int loc_rotate_y = glGetUniformLocation(shader, "u_rotate_y_matrix");
     glUniformMatrix4fv(loc_rotate_y, 1, GL_FALSE, rotate_y_matrix);
-
-    int loc_rotate_z = glGetUniformLocation(shader, "u_rotate_z_matrix");
     glUniformMatrix4fv(loc_rotate_z, 1, GL_FALSE, rotate_z_matrix);
-
-}
-
-
-void DrawCube(GLfloat centerPosX, GLfloat centerPosY, GLfloat centerPosZ, GLfloat edgeLength)
-{
-    GLfloat halfSideLength = edgeLength * 0.5f;
-
-    GLfloat vertices[] =
-    {
-        // front face
-        centerPosX - halfSideLength, centerPosY + halfSideLength, centerPosZ + halfSideLength, // top left
-        centerPosX + halfSideLength, centerPosY + halfSideLength, centerPosZ + halfSideLength, // top right
-        centerPosX + halfSideLength, centerPosY - halfSideLength, centerPosZ + halfSideLength, // bottom right
-        centerPosX - halfSideLength, centerPosY - halfSideLength, centerPosZ + halfSideLength, // bottom left
-
-        // back face
-        centerPosX - halfSideLength, centerPosY + halfSideLength, centerPosZ - halfSideLength, // top left
-        centerPosX + halfSideLength, centerPosY + halfSideLength, centerPosZ - halfSideLength, // top right
-        centerPosX + halfSideLength, centerPosY - halfSideLength, centerPosZ - halfSideLength, // bottom right
-        centerPosX - halfSideLength, centerPosY - halfSideLength, centerPosZ - halfSideLength, // bottom left
-
-        // left face
-        centerPosX - halfSideLength, centerPosY + halfSideLength, centerPosZ + halfSideLength, // top left
-        centerPosX - halfSideLength, centerPosY + halfSideLength, centerPosZ - halfSideLength, // top right
-        centerPosX - halfSideLength, centerPosY - halfSideLength, centerPosZ - halfSideLength, // bottom right
-        centerPosX - halfSideLength, centerPosY - halfSideLength, centerPosZ + halfSideLength, // bottom left
-
-        // right face
-        centerPosX + halfSideLength, centerPosY + halfSideLength, centerPosZ + halfSideLength, // top left
-        centerPosX + halfSideLength, centerPosY + halfSideLength, centerPosZ - halfSideLength, // top right
-        centerPosX + halfSideLength, centerPosY - halfSideLength, centerPosZ - halfSideLength, // bottom right
-        centerPosX + halfSideLength, centerPosY - halfSideLength, centerPosZ + halfSideLength, // bottom left
-
-        // top face
-        centerPosX - halfSideLength, centerPosY + halfSideLength, centerPosZ + halfSideLength, // top left
-        centerPosX - halfSideLength, centerPosY + halfSideLength, centerPosZ - halfSideLength, // top right
-        centerPosX + halfSideLength, centerPosY + halfSideLength, centerPosZ - halfSideLength, // bottom right
-        centerPosX + halfSideLength, centerPosY + halfSideLength, centerPosZ + halfSideLength, // bottom left
-
-        // top face
-        centerPosX - halfSideLength, centerPosY - halfSideLength, centerPosZ + halfSideLength, // top left
-        centerPosX - halfSideLength, centerPosY - halfSideLength, centerPosZ - halfSideLength, // top right
-        centerPosX + halfSideLength, centerPosY - halfSideLength, centerPosZ - halfSideLength, // bottom right
-        centerPosX + halfSideLength, centerPosY - halfSideLength, centerPosZ + halfSideLength  // bottom left
-    };
-
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    //glColor3f( colour[0], colour[1], colour[2] );
-    glEnableClientState(GL_VERTEX_ARRAY);
-    glVertexPointer(3, GL_FLOAT, 0, vertices);
-
-    glDrawArrays(GL_QUADS, 0, 24);
-
-    glDisableClientState(GL_VERTEX_ARRAY);
 }
